1b1ac84c8d4286e571d6af57820a751b0c6b663d
Robb Northrup <robbnorthrup@outlook.com>
2024-11-09 01:30:32 -0600

    packet classes update 1
    use std::array, fixed size, finalized methods, added getters

--- packet.h
+++ packet.h
@@ -1,29 +1,37 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <array>
 
-class packet {
+class Packet {
     public:
-        unsigned int pacId;
-
-        packet(unsigned int newPacId); //Only for testing purposes
-        packet(unsigned int newPacId, float gpsData[], float alt, float highTemp, float lowTemp);
-        packet(unsigned int newPacId, float gpsData[], float alt, float tempData[]);
+        // packet(unsigned int newPacId); //Only for testing purposes
+        // packet(unsigned int newPacId, float gpsData[], float alt, float tempData[]);
+        Packet(unsigned int cPacId, std::array<float, 2> cGpsData, float cAlt, float cHighTemp, float cLowTemp)
+            : pacId(cPacId), gpsData(cGpsData), alt(cAlt), highTemp(cHighTemp), lowTemp(cLowTemp) {};
         int send();
+
+        // GETTERS
+        unsigned int get_id() {return pacId;}
+        std::array<float, 2> get_gps_data() {return gpsData;}
+        float get_alt() const {return alt;}
+        float get_high_temp() const {return highTemp;}
+        float get_low_temp() const {return lowTemp;};
+        void print() const; // Print, getter
+
     private:
+        const unsigned int pacId;
+        std::array<float, 2> gpsData; // Fixed-size array for latitude and longitude
+        float alt;
+        float highTemp;
+        float lowTemp;
+
         bool handshake();
-    protected:
 };
 
-packet::packet(unsigned int newPacId, float gpsData[], float alt, float highTemp, float lowTemp) {
-    pacId = newPacId;
-    printf("Created Packet %d\n", pacId);
-}
-
-packet::packet(unsigned int newPacId, float gpsData[], float alt, float tempData[]) {
-    pacId = newPacId;
-    printf("Created Packet %d\n", pacId);
-}
-
-packet::packet(unsigned int newPacId) {
-    pacId = newPacId;
-    printf("Created Packet %d\n", pacId);
+void Packet::print() const {
+    std::cout << "Packet ID: " << pacId << "\n";
+    std::cout << "GPS: (" << gpsData[0] << ", " << gpsData[1] << ")\n";
+    std::cout << "Altitude: " << alt << "\n";
+    std::cout << "High temp: " << highTemp << "\n";
+    std::cout << "Low temp: " << lowTemp << "\n";
 }
