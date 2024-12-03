6e93f9cdf46cd63c65dd7248142623bb45b10fcb
Robb Northrup <robbnorthrup@outlook.com>
2024-12-03 14:07:31 -0800

    Thread 3 Implemented for Drone (Sending packet data)
    Need to update thread 2 + 3 to correctly send data

--- Thermal.py
+++ Thermal.py
@@ -4,16 +4,17 @@ print("test")
 # SPDX-License-Identifier: MIT
 
 import time
-import board
-import busio
-import adafruit_mlx90640
+## import board
+## import busio
+## import adafruit_mlx90640
 import multiprocessing as mp
-import ctypes
-import pyserial # for serial communication over usb
+# import ctypes
+import serial # for serial communication over usb
 from thermal_data import thermal_data
-from '\packet_class\\_v2\\packet.py' import Packet
+from radio.packet_class._v2.packet import Packet
 
-packet_lib = ctypes.CDLL('./packet_class/packet.so')
+# packet_lib = ctypes.CDLL('./packet_class/packet.so')
+rf_serial = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
 
 # Take thermal data, add GPS + alt data
 def data_structure_builder(q,q2):
@@ -30,33 +31,45 @@ def data_processing(q2,q3):
 
 # Compartmentalize data in packet, serialize, and send
 def send_packet(q3):
-	while True:
-		if q3.empty() == False:
-			print("Data on thread 3")
-			data = q3.get()
-			
-			
-            # Call the cpp function with the data 
-			packet = packet_lib.create_packet(ctypes.c_uint(), (ctypes.c_float * 2)(*data.gps), ctypes.c_float(data.barometric), ctypes.c_float(data.max_temp), ctypes.c_float(data.min_temp))
-			
-            # packet data -> byte array, send over radio
-			byte_packet = bytes(packet.contents)
-			# send_radio(byte_packet) # IMPLEMENT MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-			print('Packet sent: ', byte_packet)
+    while True:
+        if not q3.empty():
+            print("Data on thread 3")
+
+            # Get data from the queue
+            data = q3.get()
+
+            # Create a Packet object
+            packet = Packet(
+                pac_id=data.packet_id,         # Example: unique ID
+                gps_data=data.gps,             # GPS coordinates [latitude, longitude]
+                alt=data.barometric,           # Altitude in meters
+                high_temp=data.max_temp,       # Max temperature
+                low_temp=data.min_temp         # Min temperature
+            )
+
+            # Serialize the Packet
+            serialized_packet = packet.serialize()
+
+            # Send the serialized packet over RF
+            try:
+                rf_serial.write(serialized_packet)  # Send bytes over the RF module
+                print(f'Packet sent: {serialized_packet.hex()}')  # Print as hex for readability
+            except serial.SerialException as e:
+                print(f"Failed to send packet: {e}")
 
 
 if __name__ == '__main__':
 	PRINT_TEMPERATURES = True
 	PRINT_ASCIIART = False
 
-	i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+	## i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 	# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
-	mlx = adafruit_mlx90640.MLX90640(i2c)
+	## mlx = adafruit_mlx90640.MLX90640(i2c)
 	print("MLX addr detected on I2C")
-	print([hex(i) for i in mlx.serial_number])
+	## print([hex(i) for i in mlx.serial_number])
 
-	mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
+	## mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
 	frame = [0] * 768
 	mp.set_start_method('spawn')
 	q = mp.Queue()
@@ -73,7 +86,7 @@ if __name__ == '__main__':
 	while True:
 		#stamp = time.monotonic()
 		try:			
-			mlx.getFrame(frame)
+			## mlx.getFrame(frame)
 			q.put(frame)
 		except ValueError:
 			continue
