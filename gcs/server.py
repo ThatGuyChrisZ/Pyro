290d89862e78b431c674e2a1d895d522eda8d500
Chris <thatguynamedchriszinser@gmail.com>
2025-03-02 23:43:03 -0800

    Integration of Avionics into database
    ADdded avionics telemetry to database. This tracks:
    
    -Satellite count
    -GPS coordinates
    -Altitude
    -Attitude
    -Heading

--- gcs/server.py
+++ gcs/server.py
@@ -2,7 +2,13 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs, urlparse
 import json
 import sqlite3
+import multiprocessing as mp
 import pandas as pd
+from pymavlink import mavutil
+import time
+import math
+
+
 from database import (
     init_db,
     process_packet,
@@ -12,6 +18,67 @@ from database import (
     sync_to_firebase,
     update_mission_data
 )
+
+def recursive_listen(QGroundControl):
+    QGroundControl.wait_heartbeat()
+    Attitude = QGroundControl.recv_match(type='ATTITUDE', blocking=True)
+    Altitude = QGroundControl.recv_match(type='ALTITUDE', blocking=True)
+    Heading = QGroundControl.recv_match(type='VFR_HUD', blocking=True)
+    GPS = QGroundControl.recv_match(type='GPS_RAW_INT', blocking=True)
+    #GPS_RAW_INT
+    mailbox = QGroundControl.messages.keys()
+    enable = 1
+    if enable == 2:
+        for mail in mailbox:
+            print("____________________________________")
+            print(mail)
+    
+    if enable == 1:
+        print("_______________________________")
+        print("Pitch: ", Attitude.pitch)
+        print("Roll:", Attitude.roll)
+        print("Yaw:", Attitude.yaw)
+        print("Altitudee",Altitude.altitude_amsl)
+        print("Heading: ",Heading.heading)
+        print("Ground Speed: ",Heading.groundspeed)
+        print("GPS lat: ", GPS.lat)
+        print("GPS lon: ", GPS.lon)
+        print("GPS Satelites: ", GPS.satellites_visible)
+        new_time = time.time()
+        local_time = time.localtime(new_time)
+        rounded_time = round(float(new_time),1)
+        print("Time_Stamp", rounded_time)
+        
+        
+    recursive_listen(QGroundControl)
+
+def avionics_integration(output):
+    enabled = 1
+    while TRUE:
+        
+        if enabled = 1:
+            enabled = 0
+            QGroundControl.wait_heartbeat()
+            Attitude = QGroundControl.recv_match(type='ATTITUDE', blocking=True)
+            Altitude = QGroundControl.recv_match(type='ALTITUDE', blocking=True)
+            Heading = QGroundControl.recv_match(type='VFR_HUD', blocking=True)
+            GPS = QGroundControl.recv_match(type='GPS_RAW_INT', blocking=True)
+            #GPS_RAW_INT
+            mailbox = QGroundControl.messages.keys()
+            
+            export =  '{ "Attitude":Attitude, "Heading":heading, "Altitude":Altitude, "lat": GPS.lat, "lon": GPS.lon,"sat_count":,GPS.satellites_visible}'
+        
+        
+        
+            #Re enable loop
+            output.put(export)
+            enabled = 1
+            
+        
+ 
+
+
+
 
 # used for test data, reads packets as lines in txt file
 def import_packets_from_file(file_path):
@@ -179,6 +246,14 @@ class NavigationHandler(SimpleHTTPRequestHandler):
 
 # Run the server
 if __name__ == "__main__":
+
+    #Avionics Sub routine start
+    q1 = mp.Queue()
+    p1 = mp.Process(target=avionics_integration, args=(q1))
+    
+    ##
+    
+    
     host_name = "localhost"
     port_number = 8000
     server = HTTPServer((host_name, port_number), NavigationHandler)
