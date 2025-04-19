74cd1f83f56595fb2339e1a8b62920eaa99bca8f
Robnudu79 <robbnorthrup@outlook.com>
2025-04-18 22:11:57 -0700

    fixed rf_serial bug

--- drone/run.sh
+++ drone/run.sh
@@ -1,1 +1,1 @@
-/home/pi/Desktop/pip/bin/python3 /home/pi/Desktop/pip/bin/Thermal/Pixhawk_Integration/Pyro/drone/main.py
+/home/pi/Desktop/pip/bin/python3 /home/pi/Desktop/pip/bin/Thermal/main/Pyro/drone/main.py --mode 1
